892b42e04f46409cea58dd764c0cd92c7c1f1b0d
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-09-12 14:26:37 +0200

    Add project files.

---
+++ inlämmning uppgift/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+namespace Uppgift_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("detta är en kommentar");
+
+                // Detta är en kommentar på en rad och läses inte av datorn
+
+
+        }
+    }
+ }
+
+
+
+
+
+
